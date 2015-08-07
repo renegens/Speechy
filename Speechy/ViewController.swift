82d8b0b2b38fbab645bc8890665b259743275cb4
renegens <renegens@gmail.com>
2015-08-07 04:04:01 +0300

    Initial
    Initial

---
+++ Speechy/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Speechy
+//
+//  Created by Giwrgos Gens on 07/08/15.
+//  Copyright © 2015 Giwrgos Gens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordButton(sender: UIButton) {
+        //TODO: show text recording
+        //TODO: recrod user voice
+        print("Record button pressed")
+    }
+
+}
+
